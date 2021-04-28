09ba665ef0b3f0aa228dfbb23b54cef9efaa7114
Adam Koprowski <adamkoprowski4@gmail.com>
2021-04-28 21:56:04 +0200

    print shoping list

--- task1.py
+++ task1.py
@@ -2,3 +2,7 @@ shopping_dict = {
     "piekarnia": ["chleb", "pączek", "bułki"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+for key in shopping_dict:
+    values = [value.capitalize() for value in shopping_dict[key]]
+    print("Idę do ", key.capitalize(),", kupuję tu następujące  rzeczy :", values)
