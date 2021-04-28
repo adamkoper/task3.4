fb456b7079619389b09bda612e99e97206aafbb9
Adam Koprowski <adamkoprowski4@gmail.com>
2021-04-28 21:58:32 +0200

    print sum of produckt

--- task1.py
+++ task1.py
@@ -6,3 +6,10 @@ shopping_dict = {
 for key in shopping_dict:
     values = [value.capitalize() for value in shopping_dict[key]]
     print("Idę do ", key.capitalize(),", kupuję tu następujące  rzeczy :", values)
+
+count = 0
+# using isinstance
+for x in shopping_dict:
+   if isinstance(shopping_dict[x], list):
+      count += len(shopping_dict[x])
+print("W sumie kupuję ",count," produktów.")
